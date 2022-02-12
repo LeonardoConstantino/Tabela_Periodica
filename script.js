f93e9f236328876c694e5a39ad89beae38d9b8c1
Leonardo Henrique Constantino <76814929+LeonardoConstantino@users.noreply.github.com>
2022-02-12 18:27:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+let linha = document.querySelectorAll(".linha")
+let quatEle = 162
+let DIVELE
+
+const criaElementos = () => {
+    for (let i = 1; i < quatEle+1; i++) {
+        DIVELE = document.createElement("div")
+        DIVELE.setAttribute('class', 'elementos')
+
+        if (i<=18) {linha[0].appendChild(DIVELE)}
+        if (i>1 && i<=17) { 
+            DIVELE.setAttribute('class', 'branco')
+            DIVELE.innerHTML = ""
+        }
+        
+        if (i>=19 && i<=18*2) {linha[1].appendChild(DIVELE)}
+        if (i>20 && i<=30) { 
+            DIVELE.setAttribute('class', 'branco')
+            DIVELE.innerHTML = ""
+        }
+
+        if (i>=37 && i<=18*3) {linha[2].appendChild(DIVELE)}
+        if (i>38 && i<=48) { 
+            DIVELE.setAttribute('class', 'branco')
+            DIVELE.innerHTML = ""
+        }
+
+        if (i>=55 && i<=18*4) {linha[3].appendChild(DIVELE)}
+        if (i>=73 && i<=18*5) {linha[4].appendChild(DIVELE)}
+
+        if (i>=91 && i<=18*6) {linha[5].appendChild(DIVELE)}
+        if (i==93) { 
+            DIVELE.setAttribute('class', 'branco')
+            DIVELE.innerHTML = "<h2>57-71</h2><p>*</p>"
+        }
+
+        if (i>=109 && i<=18*7) {linha[6].appendChild(DIVELE)}
+        if (i==111) { 
+            DIVELE.setAttribute('class', 'branco')
+            DIVELE.innerHTML = "<h2>89-103</h2><p>**</p>"
+        }
+
+        if (i>=127 && i<=18*8) {linha[7].appendChild(DIVELE)}
+        if (i>126 && i<=129) { 
+            DIVELE.setAttribute('class', 'branco')
+            linha[7].classList.add('margin')
+            DIVELE.innerHTML = ""
+        }
+        if (i==129) {DIVELE.innerHTML = "*"}
+
+
+        if (i>=145 && i<=18*9) {linha[8].appendChild(DIVELE)}
+        if (i>144 && i<=147) { 
+            DIVELE.setAttribute('class', 'branco')
+            DIVELE.innerHTML = ""
+        }
+        if (i==147) {DIVELE.innerHTML = "**"}
+
+    }
+}
+
+criaElementos()
+
+const MOSTRA_ELEMENTOS_NA_TELA = (elemento, dados) => {
+    let elementos = document.querySelectorAll(".elementos")
+
+    elementos[elemento].innerHTML = `
+    <h2>${dados[elemento].numeroAtomico}</h2>
+    <p>${dados[elemento].simbolo}</p>
+    <span>
+    <span class="tooltip">
+        <span class="tooltiptext">
+            Nome: ${dados[elemento].nome}</br>
+            Massa atômica: ${dados[elemento].massaAtomica}
+        </span>
+    </span></span>
+    `
+}
+
+const PEGA_ELEMENTOS = dados => {
+    for (var i = 0; i<dados.length; i++){
+        MOSTRA_ELEMENTOS_NA_TELA(i, dados)
+        if (i>55 && i<=70) {
+            // elementos[i].style.backgroundColor = "red"
+            MOSTRA_ELEMENTOS_NA_TELA(i, dados)
+        }
+        if (i>72 && i<=87) {
+            // elementos[i].style.backgroundColor = "blue"
+        MOSTRA_ELEMENTOS_NA_TELA(i, dados)
+        }
+        if (i>87 && i<=102) {
+            // elementos[i].style.backgroundColor = "aqua"
+        MOSTRA_ELEMENTOS_NA_TELA(i, dados)
+        }
+        if (i>102 && i<=117) {
+            // elementos[i].style.backgroundColor = "crimson"
+        MOSTRA_ELEMENTOS_NA_TELA(i, dados)
+        }
+    }
+
+    
+    console.log(dados[1])
+}
+
+
+const PEGA_DADOS_ELEMENTOS = () => {
+    fetch("elementos.json")
+        .then(resposta => {
+            return resposta.json()
+        })
+        .then(dados => {
+            PEGA_ELEMENTOS(dados)
+        })
+}
+
+
+PEGA_DADOS_ELEMENTOS()
